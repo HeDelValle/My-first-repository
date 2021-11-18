5de83b21438713cb63f1a316a658f783e0278bc3
HeDelValle <72476230+HeDelValle@users.noreply.github.com>
2021-11-17 16:36:41 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>How do you do fellow people of color</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
